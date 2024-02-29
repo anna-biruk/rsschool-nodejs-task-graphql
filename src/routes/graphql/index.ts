2b35135a181a2fdeb22e17e1e1c4dfe9a6bab88b
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-02-29 11:47:01 +0100

    implemented handler in graphql

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, graphQlSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +14,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const {query,variables} = req.body;
+      const res = await graphql({
+        schema:graphQlSchema,
+        source:query,
+        variableValues:variables,
+        contextValue: prisma
+      })
+
+      return res;
     },
   });
 };
---
+++ src/routes/graphql/types/query.ts
@@ -0,0 +1,8 @@
+import { GraphQLObjectType } from "graphql";
+
+export const QueryType = new GraphQLObjectType({
+    name: "Query",
+    fields: {
+
+    }
+})
